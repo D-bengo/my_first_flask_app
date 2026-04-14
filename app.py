d89f1fb0f9a9f5b73980331062347007eb292812
D-bengo <kichodenis31@gmail.com>
2026-04-14 21:29:13 +0300

    Done Assignment

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to Flask"
+
+@app.route("/about")
+def about():
+    return "This is my first Flask app"
+
+@app.route("/contact")
+def contact():
+    return "Contact page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
